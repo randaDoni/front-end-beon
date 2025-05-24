4b74625d2dc2d0b2a115f0d2cb409cd4c6c3e9e4
randaDoni <91381517+randaDoni@users.noreply.github.com>
2025-05-24 18:42:58 +0700

    done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
         <span>RT Maju</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon" />
+        {/* <img src="/search.svg" alt="" className="icon" />
         <img src="/app.svg" alt="" className="icon" />
         <img src="/expand.svg" alt="" className="icon" />
         <div className="notification">
           <img src="/notifications.svg" alt="" />
           <span>1</span>
-        </div>
+        </div> */}
         <div className="user">
           <img
             src="/panda.png"
